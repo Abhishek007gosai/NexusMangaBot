b9ecb233828faf9bd7d5c90a5e17df8efba734df
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-02-19 10:59:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 # Replace these with your actual Telegram API credentials or set them as environment variables.
-API_ID = os.environ.get("API_ID", "29554281")
-API_HASH = os.environ.get("API_HASH", "e19c55640388aebfc6d8ba55a9190e62")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7938043865:AAHsGXA3yfSVrGUFGGfgi8g8AKnvZif1r2k")
+API_ID = os.environ.get("API_ID", "23537462")
+API_HASH = os.environ.get("API_HASH", "c9599a5aa61ee8ca4f5e778d20c61f24")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8314065055:AAHOFOG71poQZocwqFqTvTFqd_8lu_r7Dok")
+
